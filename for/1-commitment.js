7c5f1c38c3183e3f3ad9e48d889850ff7ddf2d83
imrancoder10 <imranhossain1148@gmail.com>
2024-09-01 15:41:52 +0600

    for loop commitment

---
+++ for/1-commitment.js
@@ -0,0 +1,7 @@
+/***
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+*/
+
+for(let i = 1; i<=60; i++){
+    console.log('I will invest at least 6 hrs every single day for next 60 days!');
+}
