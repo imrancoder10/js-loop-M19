d6012ee6023a42effb47d64622c9636056cd0770
imrancoder10 <imranhossain1148@gmail.com>
2024-09-01 15:00:33 +0600

    while loop commitment

---
+++ while/commitment.js
@@ -0,0 +1,14 @@
+/***
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+*/
+let i=1;
+while(i<=60){
+    console.log(i + '. I will invest at least 6 hrs every single day for next 60 days!');
+    i++;
+}
+
+
+/*
+for(let i = 1; i<=60; i++){
+    console.log(i + '. I will invest at least 6 hrs every single day for next 60 days!');
+}*/
