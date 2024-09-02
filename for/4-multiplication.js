d4617f2dfac251fe2e572275ea430cb4f82cfd66
imrancoder10 <imranhossain1148@gmail.com>
2024-09-02 21:26:52 +0600

    for multipliction

---
+++ for/4-multiplication.js
@@ -0,0 +1,8 @@
+/*
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+ */
+
+for(let i = 1; i<=10; i++){
+    mul = 5*i;
+    console.log('5 * ' + i + ' =', mul);
+}
